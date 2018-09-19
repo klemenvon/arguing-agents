8873754427822e77e6c6c0fb278d003056f7da9e
Klemen Voncina <klemenvon@gmail.com>
2018-09-19 10:45:23 +0200

    added case where parser is run as __main__

--- Parsers/araucaria.py
+++ Parsers/araucaria.py
@@ -2,8 +2,11 @@
 import json
 import os
 
-path_to_corpus = os.getcwd() + '/Corpora/araucaria/'
+path_to_corpus = 'Corpora/araucaria/'
 
+if __name__ == "__main__":
+	# if this is run as the main file, the path to corpus is different than when its run from the main file
+	path_to_corpus = '../Corpora/araucaria'
 
 class t_lPair():
 	# object for a training/label pair
@@ -12,13 +15,12 @@ class t_lPair():
 		self.parse()
 
 	def parse(self):
-		# takes text and json files and parses them into two data points in this object
-		# text first
 		base_path = path_to_corpus + "nodeset" + str(self.ID)
+		# storing plain text sentences in a string
 		text_path = base_path + ".txt"
 		with open(text_path,'r') as f:
 			self.plain = f.read()
-		# json file next, this one's a little more complex
+		# json file next, this may require additional parsing to simplify later
 		json_path = base_path + ".json"
-		with open(json_path,'r') as f:
+		with open(json_path,'r') as f: # don't use 'rb' as a read option, the json parser can't handle that
 			self.labeled = json.load(f)
