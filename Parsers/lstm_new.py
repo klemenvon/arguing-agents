616fd05ef30264e5071c351a26c8bf1fa74d1b45
Klemen Voncina <klemenvon@gmail.com>
2018-10-29 00:30:37 +0100

    got model training to work

---
+++ Parsers/lstm_new.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 17 12:46:13 2018
+@author: sandy
+"""
+
+# imports needed and logging
+import pandas as pd
+import re
+#path_to_corpus = os.getcwd() + '/Corpora/araucaria/'
+import gzip
+import os
+os.environ['KERAS_BACKEND']='tensorflow'
+import gensim
+import sklearn
+import nltk
+import pickle
+from nltk.tokenize import word_tokenize
+from keras.preprocessing.text import Tokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from gensim.models import Word2Vec
+from keras.models import Sequential
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from keras.models import model_from_json
+from keras.layers import Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
+from keras import utils
+import json
+
+import logging
+import pandas as pd
+import numpy as np
+
+class ConstWord2Vec(object):
+    def __init__(self):
+        self.training_datafr = pd.DataFrame(columns=['Input', 'Output'])
+        self.input = []
+        self.output = []
+        self.word_model = '';
+        self.word_lists = []
+        self.train_features = []
+        self.output = []
+        self.token_index = {}
+    
+    def readTsvFileAndConstructDataset(self, path_to_tsv):
+        tsv_files = [pos_tsv for pos_tsv in os.listdir(path_to_tsv) if pos_tsv.endswith('.tsv')]
+        csv_files = [pos_tsv for pos_tsv in os.listdir(path_to_tsv) if pos_tsv.endswith('.csv')]
+        word_lem = WordNetLemmatizer()
+        for index, tsv_each_path in enumerate(tsv_files):
+            if tsv_each_path != "school_uniforms.tsv":
+                print("Reading file "+tsv_each_path)
+                train_file = pd.read_csv(os.path.join(path_to_tsv, tsv_each_path), delimiter='\t',encoding='utf-8', error_bad_lines=False)
+                for row_index, each_train in train_file.iterrows():
+                    regex_sen = re.sub('[^a-zA-z0-9\s]', '', each_train[4])
+                    regex_sen = regex_sen.lower()
+                    word_splits = word_tokenize(regex_sen)
+                    word_splits = [word_lem.lemmatize(i, pos='v') for i in word_splits]
+                    word_splits = [word_lem.lemmatize(i) for i in word_splits]
+                    recon_sen = ' '.join(word for word in word_splits)
+                    #considering only sentences having word length > 3
+                    if len(word_splits) > 3:
+                        self.word_lists.append(recon_sen)
+                        if each_train[5] == 'NoArgument':
+                            self.output.append('0')
+                        else:
+                            self.output.append('1') 
+                    
+        for index, csv_each_path in enumerate(csv_files):
+            print(csv_files)
+            train_file = pd.read_csv(os.path.join(path_to_tsv, csv_each_path), delimiter=',',encoding='utf-8', error_bad_lines=False)
+            print("reading file "+csv_each_path)
+            for row_index, each_train in train_file.iterrows():
+                regex_sen = re.sub('[^a-zA-z0-9\s]', '', each_train[4])
+                regex_sen = regex_sen.lower()
+                word_splits = word_tokenize(regex_sen)
+                word_splits = [word_lem.lemmatize(i, pos='v') for i in word_splits]
+                recon_sen = ' '.join(word[0] for word in word_splits)
+                #considering only sentences having word length > 3
+                if len(word_splits) > 3:
+                    self.word_lists.append(recon_sen)
+                    if each_train[5] == 'NoArgument':
+                        self.output.append('0')
+                    else:
+                        self.output.append('1') 
+        
+        keras_Tokenizer = Tokenizer()
+        keras_Tokenizer.fit_on_texts(self.word_lists)
+        encoded_sens = keras_Tokenizer.texts_to_sequences(self.word_lists)
+        print(encoded_sens)
+        vocab_size = len(keras_Tokenizer.word_counts)+1
+        max_length = 150
+        batch_size = 32
+#       encoded_sens = [one_hot(d, vocab_size) for d in self.word_lists]
+#        print(encoded_sens)
+        padded_sens = pad_sequences(encoded_sens, maxlen=max_length, padding='post')
+        print(padded_sens[:5])
+        model = Sequential()
+        model.add(Embedding(vocab_size, 100, input_length=max_length))
+        model.add(LSTM(max_length, dropout_U = 0.2, dropout_W = 0.2))
+        model.add(Dense(2,activation='softmax'))
+        model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+        print(model.summary())
+        self.output = pd.get_dummies(self.output).values
+        X_train, X_valid, Y_train, Y_valid = train_test_split(padded_sens,self.output, test_size = 0.20, random_state = 36)
+        #Here we train the Network.
+        model.fit(X_train, Y_train, batch_size =batch_size, nb_epoch = 5,  verbose = 5)
+        score,acc = model.evaluate(X_valid, Y_valid, verbose = 2, batch_size = batch_size)
+        print("validation accuracy ",acc)
+        model_json = model.to_json()
+        with open('tokenizer.pickle', 'wb') as handle:
+            pickle.dump(keras_Tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open("model.json", "w") as json_file:
+            json_file.write(model_json)
+        model.save_weights("model.h5")
+        print("Saved model to disk")
+        
+    def load_model(self, sen_list, output):
+        # load json and create model
+        max_length = 150
+        batch_size = 32
+        tokenizer = ''
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("model.h5")
+        print("Loaded model from disk")
+        with open('tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
+        
+        loaded_model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+        encoded_sens = tokenizer.texts_to_sequences(sen_list)
+        print("after text to sequence")
+        padded_sens = pad_sequences(encoded_sens, maxlen=max_length, padding='post')
+        score,acc = loaded_model.evaluate(padded_sens, np.array(output), verbose = 2, batch_size = batch_size)
+        print("validation accuracy ",acc)
--- klemen_main.py
+++ klemen_main.py
@@ -1,11 +1,17 @@
 # Arguing agents start again
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+import seaborn as sns # unused, consider removing
+import matplotlib.pyplot as plt # unused, consider removing
 import os,csv, time
 import spacy # has pos tagger, word vector stuff and a bunch of other tools, very fast as well
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
 from tqdm import tqdm
+# a debugging import
+from pprint import pprint
+import json
+
 top_path = 'Corpora/complete/'
 path_to_data = os.getcwd() + '/' + top_path
 
@@ -13,6 +19,7 @@ global_timer = time.time()
 
 files = os.listdir(path_to_data)
 files = list(filter(lambda k: '.tsv' in k,files)) # keep only tsv files
+files = files[:1] # only the first file so that tests don't take hours
 
 temp_frames = []
 df = pd.DataFrame()
@@ -27,59 +34,156 @@ df = pd.concat(temp_frames,ignore_index=True)
 # adding simpler labels
 def labeling(row):
 	if row['annotation'] == 'NoArgument':
-		return 'NoArgument'
+		return 0
 	elif row['annotation'] in ['Argument_against','Argument_for']:
-		return 'Argument'
+		return 1
 # apply labels
 df['label'] = df.apply(lambda row: labeling(row),axis=1)
 
 ### Once we have the data, time to initialize the spacy parser
-model = 'en_core_web_lg'
-print("Loading model {}".format(model))
+l_model = 'en_core_web_lg'
+print("Loading model {}".format(l_model))
 start_time = time.time()
-nlp = spacy.load(model) # this is the large sized model that also supports word vectors (for some reason loads faster than medium size model)
+nlp = spacy.load(l_model) # this is the large sized model that also supports word vectors (for some reason loads faster than medium size model)
 print("Done. Loading took {} seconds".format((time.time()-start_time)))
 
+# some testing stuff
+#pprint(vars(nlp))
 
+'''
 # extract features from sentences (try different methods)
-def feature_word_vector(sentence):
+def feature_word_vector(sentence,token_counter):
 	# given a sentence, extracts word vectors
 	sentence = nlp(sentence)
-	features = pd.DataFrame()
+	features = []
+	#features = pd.DataFrame()
 	for w in sentence:
 		if(not w.is_stop and not w.pos_ == "PUNCT"):
-			features = features.append(pd.Series(w.vector),ignore_index=True)
+			features.append(w.vector)
+			token_counter.increment()
+			#features = features.append(pd.Series(w.vector),ignore_index=True)
 	return features
 
 def extract_features(row):
 	features = feature_word_vector(row['sentence'])
 	return features
+'''
 
-# create a row with all the features
-tqdm.pandas()
-df['features'] = df.progress_apply(lambda row: extract_features(row),axis=1)
+vectors = []
+raw_sentences = df['sentence'].tolist()
+labels = df['label'].tolist()
+processed_sentences = []
+max_len = 0
+
+print("Processing sentences using spaCy")
+for s in tqdm(range(len(raw_sentences))): # len(raw_sentences) goes here normally
+	processed = nlp(raw_sentences[s])
+	processed_sentences.append(processed)
+	temp_vectors = []
+	temp_max = 0
+	for word in processed:
+		if(not word.pos_ in ['PUNCT','SYM','X']): # not word.is_stop and 
+			temp_vectors.append(word.lex_id)
+			temp_max += 1
+	vectors.append(temp_vectors)
+	if(temp_max > max_len):
+		max_len = temp_max
+
+
+# more testing stuff
+#print(type(processed_sentences[1][3]))
+#print(processed_sentences[1][3])
+#print(processed_sentences[1][1].vector)
+#pprint(dir(processed_sentences[1][3]))
+#print("Lexical ID ",processed_sentences[1][3].lex_id)
+#print("Rank ",processed_sentences[1][3].rank)
+#print("Vector of the document {}".format(processed_sentences[1]._vector))
+#print(type(nlp.vocab.vectors))
+#pprint(dir(nlp.vocab.vectors))
+#print(nlp.vocab.vectors.shape)
+#print(len(nlp.vocab.vectors.data))
+#print(nlp.vocab.vectors.data[0])
+#print(nlp.vocab.vectors.data[1])
+#print("Sample key ID: {} ".format(list(nlp.vocab.vectors.key2row)[0]))
+#print("Test   key ID: {} ".format(hash(processed_sentences[1][3])))
+#print("Another keyID: {} ".format(processed_sentences[1][3].orth))
+#print("Attempting a match to row: {}".format(nlp.vocab.vectors.key2row[processed_sentences[1][3].orth]))
+
+# test matching
 '''
-for row in tqdm(range(2)):
-	features = feature_word_vector(df.iloc[row]['sentence'])
-	label = df.iloc[row]['label']
-	#data = data.append(new_entry,ignore_index=True)
+for i in range(len(processed_sentences[1])):
+	orth = processed_sentences[1][i].orth
+	rank = processed_sentences[1][i].rank
+	lex_id = processed_sentences[1][i].lex_id
+	word = processed_sentences[1][i].text
+	p = processed_sentences[1][i]
+	print("Word:	{}".format(word))
+	print("Rank:	{}".format(rank))
+	print("Lex ID:	{}".format(lex_id))
+	print("Match:	{}".format(nlp.vocab.vectors.key2row[orth]))
+	if(p.vector.all() == nlp.vocab.vectors.data[rank].all()):
+		print("Eureka!")
+	else:
+		print("No Luck...")
+	print("")
+
+print("Premature ending for testing purposes")
+exit()
 '''
-df.info()
-write_name1 = 'extracted_vectors1.json'
-write_name2 = 'extracted_vectors2.json'
-#write_path = top_path + write_name
-print("Converting Data Frame 1 to JSON")
-sub_frame1 = df[:12000]
-out1 = sub_frame1.to_json()
-print("Converting Data Frame 2 to JSON")
-sub_frame2 = df[12000:]
-out2 = sub_frame2.to_json()
-print("Writing dataframe 1 with features to {}.".format(write_name1))
-with open(write_name1,'w+') as f:
-	f.write(out1)
-print("Writing dataframe 2 with features to {}.".format(write_name2))
-with open(write_name2,'w+') as h:
-	h.write(out2)
+
+### model
+# Imports (not sure how wise it is to do those here instead of at the top)
+from keras.models import Sequential
+from keras.layers import (Embedding, Activation, LSTM, Dense,Dropout)
+from keras.utils import to_categorical
+# set backend
+os.environ['KERAS_BACKEND']='tensorflow'
+
+# some variables we're going to need for the model
+vocab_len = len(nlp.vocab.vectors.data) # just in case
+vector_len = 300
+batch_size = 32
+
+print("Maximum sequence length = {}".format(max_len))
+
+# last bit of pre-processing
+print("Padding vectors")
+padded_vectors = pad_sequences(vectors,maxlen=max_len,padding='post')
+print("Splitting data")
+labels = to_categorical(labels)
+X_train, X_test, Y_train, Y_test = train_test_split(padded_vectors,labels,test_size=0.2,random_state=42)
+
+# initialize model
+print("Initializing Keras Model")
+model = Sequential()
+model.add(Embedding(vocab_len,300,weights=[nlp.vocab.vectors.data],input_length=max_len,trainable=False))
+#model.add(Dropout(0.4))
+model.add(LSTM(max_len,dropout=0.2,recurrent_dropout=0.2,use_bias=True))
+model.add(Dense(2,activation='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+print(model.summary())
+# train the model
+print("Training model")
+model_timing = time.time()
+model.fit(X_train, Y_train, batch_size =batch_size, epochs = 100,  verbose = 2)
+print("Model Trained. Time elapsed: {}".format(time.time()-model_timing))
+
+# score model
+score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
+print('Test accuracy: {}\nModel Score: {}'.format(acc,score))
+
+# Save model to disk
+def save_model(model):
+	model_save = 'klemen_model.json'
+	print("Converting model to json")
+	model_json = model.to_json()
+	print("Saving model to {}".format(model_save))
+	with open(model_save,'w+') as json_file:
+		json_file.write(model_json)
+
+save_model(model)
+
+
 print("Done.")
 print("Total time elapsed: {}s".format((time.time()-global_timer)))
 
